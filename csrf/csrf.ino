967054a802e8be3dfce67968d1dd6f77e815d3d8
Rob Thomson <rob@marotori.com>
2020-11-13 10:58:42 +0000

    Updates to sensorts to include extras.
    Updates to sensorts to include extras.

--- csrf/csrf.ino
+++ csrf/csrf.ino
@@ -23,22 +23,31 @@
 
 //bring in all externals related to crossfire
 extern uint32_t crossfireChannels[CROSSFIRE_CHANNELS_COUNT];  //pulses data
-extern float sensorVario;
-extern double sensorGPSLat;
-extern double sensorGPSLong;
-extern float sensorAltitude;
-extern float sensorHeading;
-extern uint32_t sensorSpeed;
-extern uint32_t sensorSats;
-extern float sensorPitch;
-extern float sensorRoll;
-extern float sensorYaw;
-extern double sensorVoltage;
-extern double sensorCurrent;
-extern double sensorFuel;
-extern double sensorRSSI;
-extern double sensorSNR;
-extern double sensorTXPWR;
+double sensorGPSLat;
+double sensorGPSLong;
+float sensorHeading=0;
+float sensorAltitude=0;
+uint32_t sensorSpeed=0;
+uint32_t sensorSats=0;
+float sensorPitch=0;
+float sensorRoll=0;
+float sensorYaw=0;
+double sensorVoltage;
+double sensorCurrent;
+double sensorFuel;
+float sensorVario;
+uint32_t  sensor1RSS;
+uint32_t sensor2RSS;
+uint32_t sensorRXQly;
+uint32_t sensorRXSNR; 
+uint32_t sensorAntenna; 
+uint32_t sensorRFMode;
+uint32_t sensorTXPWR;
+uint32_t sensorTXRSSI; 
+uint32_t sensorTXQly;
+uint32_t sensorTXSNR;
+uint32_t sensorCapacity;
+
 
 
 
